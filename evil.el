0b6f049a3dff202f419f9149cb492321ef8fe9f4
Jordan King <jordan.king@soti.net>
2024-08-22 20:58:23 +0100

    Evil

---
+++ evil.el
@@ -0,0 +1,153 @@
+;;; evil.el --- >:L -*- no-byte-compile: t; lexical-binding: t; -*-
+
+;; TODO: Consultify various and load after completion
+(defvar-keymap my-buffer-prefix-keymap
+  :doc "buffers"
+  "b" #'consult-buffer
+  "k" #'kill-this-buffer
+  "d" #'kill-this-buffer
+  "p" #'previous-buffer
+  "n" #'next-buffer
+  "B" #'consult-buffer
+  "r" #'rename-buffer
+  "R" #'revert-buffer)
+
+(defvar-keymap my-file-prefix-keymap
+  :doc "Files"
+  "f" #'find-file
+  "d" #'dired-jump
+  "r" #'consult-recentf)
+
+(defvar-keymap my-project-prefix-keymap
+  :doc "Projects"
+  "f" #'project-find-file
+  "!" #'project-shell-command
+  "&" #'project-async-shell-command
+  "c" #'project-compile
+  "r" #'project-recompile
+  "d" #'project-find-dir
+  "D" #'project-dired
+  ;; "p" #'project-switch-project
+  "p" #'tabspaces-open-or-create-project-and-workspace
+  "o" #'find-sibling-file
+  "K" #'tabspaces-kill-buffers-close-workspace
+  "Q" #'tabspaces-kill-buffers-close-workspace)
+
+(defvar-keymap my-window-prefix-keymap
+  :doc "Windows"
+  "h" #'windmove-left
+  "j" #'windmove-down
+  "k" #'windmove-up
+  "l" #'windmove-right
+  "H" #'windmove-swap-states-left
+  "J" #'windmove-swap-states-down
+  "K" #'windmove-swap-states-up
+  "L" #'windmove-swap-states-right
+  "d" #'delete-window
+  "D" #'delete-other-windows
+  "s" #'split-window-vertically
+  "v" #'split-window-horizontally)
+
+(defvar-keymap my-tabs-prefix-keymap
+  :doc "Workspaces"
+  "TAB" #'tab-bar-echo-area-display-tab-names)
+
+(defvar-keymap my-leader-prefix-keymap
+  :doc leader
+  "b" my-buffer-prefix-keymap
+  "f" my-file-prefix-keymap
+  "p" my-project-prefix-keymap
+  "w" my-window-prefix-keymap
+  "SPC" #'execute-extended-command
+  ";" #'eval-expression
+  "h" help-map
+  "." #'vertico-repeat
+  "TAB" my-tabs-prefix-keymap)
+
+;; evil-want-keybinding must be declared before Evil and Evil Collection
+(setq evil-want-keybinding nil)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-undo-system 'undo-fu)
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-select-search-module 'evil-search-module 'evil-search)
+  (evil-set-leader nil (kbd "SPC"))
+  (evil-define-key 'normal 'global (kbd "<leader>") my-leader-prefix-keymap)
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package undo-fu
+  :ensure t
+  :commands (undo-fu-only-undo
+             undo-fu-only-redo
+             undo-fu-only-redo-all
+             undo-fu-disable-checkpoint)
+  :custom
+  ;; 3 times the default values
+  (undo-limit (* 3 160000))
+  (undo-strong-limit (* 3 240000)))
+
+(use-package undo-fu-session
+  :ensure t
+  :config
+  (undo-fu-session-global-mode))
+
+(use-package vim-tab-bar
+  :ensure t
+  :commands vim-tab-bar-mode
+  :config (vim-tab-bar-mode +1))
+
+(use-package vdiff
+  :ensure t
+  :defer t
+  :commands (vdiff-buffers
+             vdiff-buffers3
+             vdiff-quit
+             vdiff-files
+             vdiff-files3)
+  :custom
+  (vdiff-auto-refine t)
+  (vdiff-only-highlight-refinements t))
+
+(use-package evil-visualstar
+  :after evil
+  :ensure t
+  :defer t
+  :commands global-evil-visualstar-mode
+  :config (global-evil-visualstar-mode +1))
+
+(use-package evil-surround
+  :after evil
+  :ensure t
+  :defer t
+  :commands global-evil-surround-mode
+  :custom
+  (evil-surround-pairs-alist
+   '((?\( . ("(" . ")"))
+     (?\[ . ("[" . "]"))
+     (?\{ . ("{" . "}"))
+
+     (?\) . ("(" . ")"))
+     (?\] . ("[" . "]"))
+     (?\} . ("{" . "}"))
+
+     (?< . ("<" . ">"))
+     (?> . ("<" . ">"))))
+  :config (global-evil-surround-mode +1))
+
+;;(with-eval-after-load "evil"
+;;  (evil-define-operator my-evil-comment-or-uncomment (beg end)
+;;    "Toggle comment for the region between BEG and END."
+;;    (interactive "<r>")
+;;    (comment-or-uncomment-region beg end))
+;;  (evil-define-key 'normal 'global (kbd "gc") 'my-evil-comment-or-uncomment))
+
--- post-init.el
+++ post-init.el
@@ -5,14 +5,6 @@
 (add-hook 'after-init-hook #'savehist-mode)
 (add-hook 'after-init-hook #'save-place-mode)
 
-(use-package gcmh
-  :ensure t
-  :hook (after-init . gcmh-mode)
-  :custom
-  (gcmh-idle-delay 'auto)
-  (gcmh-auto-idle-delay-factor 10)
-  (gcmh-low-cons-threshold minimal-emacs-gc-cons-threshold))
-
 (use-package auto-compile
   :demand t
   :custom
@@ -22,10 +14,20 @@
   (auto-compile-on-load-mode)
   (auto-compile-on-save-mode))
 
+(use-package gcmh
+  :ensure t
+  :custom
+  (gcmh-idle-delay 'auto)
+  (gcmh-auto-idle-delay-factor 10)
+  (gcmh-low-cons-threshold minimal-emacs-gc-cons-threshold)
+  :config (gcmh-mode +1))
+
+(minimal-emacs-load-user-init "evil.el")
 (minimal-emacs-load-user-init "completion.el")
-(minimal-emacs-load-user-init "bindings.el")
-(minimal-emacs-load-user-init "elisp.el")
-;; (minimal-emacs-load-user-init "bindings.el")
+;;(minimal-emacs-load-user-init "bindings.el")
+;;(minimal-emacs-load-user-init "elisp.el")
+;;(minimal-emacs-load-user-init "bindings.el")
+(minimal-emacs-load-user-init "lsp.el")
 
 (setq scroll-margin 10
       mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
@@ -38,13 +40,68 @@
 
 (setq default-directory (getenv "HOME"))
 
+
+
 (set-face-attribute 'default nil :font "Berkeley Mono-10")
+(setq widget-image-enable nil)
 (use-package nano-theme
-  :vc (:url "https://github.com/rougier/nano-theme")
+  :ensure (nano-theme :host github :repo "https://github.com/rougier/nano-theme")
   :config
   (nano-light)
   (load-theme 'nano t))
 
+(use-package svg-tag-mode
+  :ensure (svg-tag-mode :host github :repo "https://github.com/rougier/svg-tag-mode")
+  :config
+  ;;(setq svg-tag-tags
+  ;;  '((":TODO:" . ((lambda (tag) (svg-tag-make " ! " :face 'nano-critical))))))
+  (global-svg-tag-mode +1))
+
+(setq project-vc-ignores '("target/" "bin/" "obj/")
+      project-vc-extra-root-markers '(".project"
+                                      "go.mod"
+                                      "Cargo.toml"
+                                      "project.clj"
+                                      "pom.xml"
+                                      "package.json"
+                                      "angular.json"
+                                      "Makefile"
+                                      "README.org"
+                                      "README.md"))
 
 
+(use-package tabspaces
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "¯\_(ツ)_/¯")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  ;; (tabspaces-initialize-project-with-todo t)
+  ;; (tabspaces-todo-file-name "project-todo.org")
+  ;; sessions
+  (tabspaces-session t)
+  ;; (tabspaces-session-auto-restore t)
+  :config
+  (setq tab-bar-show nil)
+  ;; Filter Buffers for Consult-Buffer
+  ;; hide full buffer list (still available with "b" prefix)
+  (consult-customize consult--source-buffer :hidden t :default nil)
+  ;; set consult-workspace buffer list
+  (defvar consult--source-workspace
+    (list :name     "Workspace Buffers"
+          :narrow   ?w
+          :history  'buffer-name-history
+          :category 'buffer
+          :state    #'consult--buffer-state
+          :default  t
+          :items    (lambda () (consult--buffer-query
+                           :predicate #'tabspaces--local-buffer-p
+                           :sort 'visibility
+                           :as #'buffer-name)))
+  
+    "Set workspace buffer list for consult-buffer.")
+  (add-to-list 'consult-buffer-sources 'consult--source-workspace)
 
+  (tabspaces-mode))
